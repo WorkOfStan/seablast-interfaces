076100b641623eafef4dbda011070f8fb6814c12
WorkOfStan <rejthar@stanislavrejthar.com>
2026-06-28 00:31:10 +0200

    chore: remove `Options -Indexes` from .htaccess (#16)
    ### Changed
    
    - Document unauthenticated identity recommendations and narrow package wording to the current interface contract.
    
    ### Removed
    
    - Remove `Options -Indexes` from .htaccess as preventing the directory listing is the responsibility of the app

--- src/IdentityManagerInterface.php
+++ src/IdentityManagerInterface.php
@@ -14,6 +14,9 @@ interface IdentityManagerInterface
     /**
      * Return the list of groups to which the user belongs. It may be empty.
      *
+     * If the user is not authenticated, implementations may return an empty
+     * array or throw an exception.
+     *
      * @return array<int>
      */
     public function getGroups(): array;
@@ -21,12 +24,18 @@ interface IdentityManagerInterface
     /**
      * Return the user's role id.
      *
+     * If the user is not authenticated, implementations are recommended to
+     * throw an exception.
+     *
      * @return int
      */
     public function getRoleId(): int;
 
     /**
      * Return the user's id.
+     *
+     * If the user is not authenticated, implementations are recommended to
+     * throw an exception.
      *
      * @return int
      */
